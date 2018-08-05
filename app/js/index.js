aff5d5c89c906dea90df570964aeb703dbfade23
 小玲欢 <0@eexx.me>
2018-08-05 21:06:46 +0800

    提交

---
+++ app/js/index.js
@@ -0,0 +1,13 @@
+
+import React from "react"
+import Reactdom from "react-dom"
+import AppCompent from "./AppCompent"
+
+
+
+
+Reactdom.render(
+	<AppCompent />,
+	document.getElementById("root")
+);
+
