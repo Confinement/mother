be6dc7913932cd4e85badb3b8b8526c33343b82c
 小玲欢 <0@eexx.me>
2018-08-08 16:00:23 +0800

    发布需求 时间修正

--- app/common/Address.js
+++ app/common/Address.js
@@ -17,6 +17,7 @@ arryAddress = function initAddress(){
 	}).then(response => response.json()).then(function(res) {
 		
 		if(res.code==="100000"){
+			// 地址为空要跳转到新增加地址。
 			addressList=res.content
 		}else{
 			alert(res.desc)
@@ -33,7 +34,7 @@ class Address extends React.component{
 			addressList:arryAddress,
 		})
 	}
-	
+
 	render(){
 		return(
 			<div></div>
