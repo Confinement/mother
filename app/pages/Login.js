84c06ae77593a111913bf7dc3a481a24110a1e9e
 小玲欢 <0@eexx.me>
2018-08-09 11:38:10 +0800

    添加手机号校验

--- app/pages/Login.js
+++ app/pages/Login.js
@@ -19,7 +19,41 @@ class Login extends React.Component{
 		this.setState({
 			pawValue: event.target.value,
 		});
-	}
+  }
+  //手机号判断
+  telCheck(event){
+    this.tel=event.target.value
+    console.log(this.tel)
+    var reg=/^1[34578]\d{9}$/;
+    if(reg.test(this.tel)==false){
+        this.setState({
+           telError:"请输入正确的手机号"
+        })
+    }else{
+        this.setState({
+            telError:""
+        })
+    }
+
+}
+//密码判断
+passwordCheck(event){
+    this.password=event.target.value
+    var reg=/^\w{6,20}$/;
+    if(reg.test(this.password)==false){
+        this.setState({
+            passwordError:"密码为6-20位数字或字母或下划线!"
+        })
+    }else{
+        this.setState({
+            passwordError:""
+        })
+        }
+
+}
+
+
+
   /**
     发送验证码
     Platform:1.月嫂-Android;2.月嫂-Ios;3.妈妈-Android;4.妈妈-Ios;5.月嫂-微信小程序;
@@ -105,7 +139,7 @@ class Login extends React.Component{
 	render(){
 		return (
 			<div>
-				<input type="text" className="login-ph" placeholder="请输入手机号码" onChange={this.aw.bind(this)} value={this.state.phValue}/>
+				<input type="text" className="login-ph" placeholder="请输入手机号码" onChange={this.aw.bind(this)} value={this.state.phValue} onBlur={(event)=>this.telCheck(event)}/>
 				<input type="text" className="login-pawd" onChange={this.pawChange.bind(this)} value={this.state.pawValue}/><button onClick={this.handleSendSms.bind(this)}>获取验证码</button>
 				<p>未注册的手机号验证后自动创建月亮账号</p>
 				<button >登录</button>
